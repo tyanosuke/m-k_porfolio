90e566df7f603fa2cbce7a2e0f83e01f471b563d
tyanosuke <128273659+tyanosuke@users.noreply.github.com>
2023-04-03 12:49:15 +0900

    Add files via upload

--- script/controller/controller-gallery.js
+++ script/controller/controller-gallery.js
@@ -5,9 +5,18 @@ $(document).ready(function () {
   const body = document.querySelector("body");
   body.style.backgroundColor = "#" + sessionStorage.getItem("color");
 
+  // イラストのファイルネームを取得
+  const imageFileName = sessionStorage.getItem("image");
+
+  // キャプションを表示
+  const caption = document.getElementById("caption");
+  const dotIndex = imageFileName.lastIndexOf(".");
+  caption.src =
+    imageFileName.slice(0, dotIndex) + "_c" + imageFileName.slice(dotIndex);
+
   // イラストを表示
-  const item = document.getElementById("displayItem");
-  item.src = sessionStorage.getItem("image");
+  const displayItem = document.getElementById("displayItem");
+  displayItem.src = imageFileName;
 
   // ================================================================
 });
