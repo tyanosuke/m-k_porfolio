8019601e5bed7910fc9c609f150d26bf9dde3e4f
tyanosuke <128273659+tyanosuke@users.noreply.github.com>
2023-04-05 16:12:18 +0900

    Add files via upload

--- script/controller/controller-main.js
+++ script/controller/controller-main.js
@@ -5,6 +5,8 @@ window.onpageshow = function (event) {
   }
 };
 
+const numImagesToShow = 15; // 表示する画像の数
+
 $(document).ready(function () {
   /**
    * 初期化
@@ -21,7 +23,6 @@ $(document).ready(function () {
     .then((data) => {
       // 3. リストからランダムにいくつかのファイル名を抜き出す。
       const randomFilenames = [];
-      const numImagesToShow = 15; // 表示する画像の数
       while (randomFilenames.length < numImagesToShow) {
         const randomIndex = Math.floor(Math.random() * data.length);
         const randomFilename = data[randomIndex];
@@ -92,6 +93,9 @@ function moveElement(element) {
 
 // 設定
 function setMove(element) {
+  // 現在座標を取得
+  const rect = element.getBoundingClientRect();
+
   // Ｘ座標
   const randomLeft = Math.floor(Math.random() * window.innerWidth);
   element.style.left = randomLeft + "px";
@@ -101,19 +105,18 @@ function setMove(element) {
   element.style.top = randomTop + "px";
 
   // Z座標
-  const randomPriority = Math.floor(Math.random() * 10);
+  const randomPriority = Math.random() * 10 * numImagesToShow;
   element.style.zIndex = randomPriority;
 
   // 大きさ
   const scale = Math.random() * 1.5 + 0.25;
-  element.style.width = "calc(20% * " + scale + ")";
+  element.style.width = "calc(30vw * " + scale + ")";
 
   // 角度
   const angle = Math.floor(Math.random() * 360);
   element.style.transform = "rotate(" + angle + "deg)";
 
   // トランジション
-  const rect = element.getBoundingClientRect();
   const dist =
     Math.abs(randomLeft - rect.left) + Math.abs(randomTop - rect.top);
   const speed = 50;
