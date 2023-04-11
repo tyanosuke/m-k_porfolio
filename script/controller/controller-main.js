bfcd7470454149d7d1c22f2cd8524657b77e99a9
tyanosuke <128273659+tyanosuke@users.noreply.github.com>
2023-04-11 10:22:55 +0900

    Add files via upload

--- script/controller/controller-main.js
+++ script/controller/controller-main.js
@@ -34,6 +34,7 @@ $(document).ready(function () {
 
       // 4. 抜き出したファイル名を使用して、画像を表示する。
       randomFilenames.forEach((data) => {
+        // 要素作成
         const item = document.createElement("div");
         item.classList.add("item");
         const image = document.createElement("img");
@@ -42,9 +43,14 @@ $(document).ready(function () {
         image.setAttribute("data-cap", folderPath + "/" + data.name + "_c");
         image.setAttribute("data-col", data.color);
         item.appendChild(image);
-        setMove(item);
-        moveElement(item);
         imageContainer.appendChild(item);
+
+        // ランダム移動
+        moveElement(item);
+        // 次の移動をスケジュールする
+        item.addEventListener("transitionend", () => {
+          moveElement(item);
+        });
       });
     })
 
@@ -78,50 +84,53 @@ $(document).ready(function () {
 // ================================================================
 
 const maxDuration = 5000;
-const moveSpeed = 100;
+const moveSpeed = 300;
 const moveMax = (maxDuration / 1000) * moveSpeed;
 
 // 動き設定
 function moveElement(element) {
-  // 移動間隔をランダムに決定する
-  const interval = Math.floor(Math.random() * maxDuration);
-
-  // 次の移動をスケジュールする
-  setTimeout(() => {
-    setMove(element);
-    moveElement(element);
-  }, interval);
+  // ランダム移動
+  setMove(element);
 }
 
 // 設定
 function setMove(element) {
   // 現在座標を取得
   const rect = element.getBoundingClientRect();
+  const screen = document.querySelector(".container").getBoundingClientRect();
+  const screenWidth = screen.width;
+  const screenHeight = screen.height;
+
+  let max;
+  let min;
 
   // Ｘ座標
-  const randomLeft = Math.floor(Math.random() * window.innerWidth);
+  max = screenWidth;
+  min = 10;
+  const randomLeft = Math.floor(Math.random() * (max - min) + min);
   element.style.left = randomLeft + "px";
 
   // Ｙ座標
-  const randomTop = Math.floor(Math.random() * window.innerHeight);
+  max = screenHeight;
+  min = 10;
+  const randomTop = Math.floor(Math.random() * (max - min) + min);
   element.style.top = randomTop + "px";
 
-  // Z座標
-  const randomPriority = Math.random() * 10 * numImagesToShow;
-  element.style.zIndex = randomPriority;
-
   // 大きさ
-  const scale = Math.random() * 1.5 + 0.25;
-  element.style.width = "calc(30vw * " + scale + ")";
+  if (!element.style.width) {
+    max = 2.0;
+    min = 0.25;
+    const scale = Math.random() * (max - min) + min;
+    element.style.width = "calc(20vw * " + scale + ")";
+  }
 
   // 角度
   const angle = Math.floor(Math.random() * 360);
   element.style.transform = "rotate(" + angle + "deg)";
 
   // トランジション
-  const dist = Math.sqrt(
-    Math.pow(randomLeft - rect.left, 2) + Math.pow(randomTop - rect.top, 2)
-  );
-  const time = Math.max(dist / moveSpeed, 1);
-  element.style.transition = "all " + time + "s";
+  const dist =
+    Math.abs(randomLeft - rect.left) + Math.abs(randomTop - rect.top);
+  const time = dist / moveSpeed;
+  element.style.transition = "all " + time + "s linear";
 }
